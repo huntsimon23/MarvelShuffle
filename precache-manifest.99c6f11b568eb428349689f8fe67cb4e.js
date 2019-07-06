7a5bd46a60e8e1ae7e388ad55ecb40f6d22d913b
huntsimon23 <hunt.simon23@gmail.com>
2019-07-05 23:40:23 -0400

    Updates

---
+++ precache-manifest.99c6f11b568eb428349689f8fe67cb4e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4968f9ca6e07aa40b6015d6134243ef9",
+    "url": "/MarvelShuffle/index.html"
+  },
+  {
+    "revision": "f6b4327ae95b452349cd",
+    "url": "/MarvelShuffle/static/css/main.b47cde58.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/MarvelShuffle/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "f6b4327ae95b452349cd",
+    "url": "/MarvelShuffle/static/js/main.cc06e3bc.chunk.js"
+  },
+  {
+    "revision": "9999711ef76073203195",
+    "url": "/MarvelShuffle/static/js/runtime~main.2a40e4c1.js"
+  }
+]);
